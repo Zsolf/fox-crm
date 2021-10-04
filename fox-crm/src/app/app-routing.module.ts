6fc60b1862bb32dcb4b8fb0ded52293366a9888e
Zsolf <zsolf57@gmail.com>
2021-10-04 23:58:49 +0200

    FCRM-003: Routing inicializalas

--- fox-crm/src/app/app-routing.module.ts
+++ fox-crm/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CompanyDataPageComponent } from './pages/main-page/company-data-page/company-data-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: 'company', component: CompanyDataPageComponent },
+  { path: '**', component: CompanyDataPageComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
