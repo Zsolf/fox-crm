f52e3357a8afe464244e6a7376d3edf0cef04308
Zsolf <zsolf57@gmail.com>
2021-10-10 00:07:19 +0200

    FCRM-002: Ertekesites oldal init

--- fox-crm/src/app/app-routing.module.ts
+++ fox-crm/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { CompanyPageComponent } from './pages/main-page/company-page.component';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { SalePageComponent } from './pages/sale-page/sale-page.component';
 
 const routes: Routes = [ 
   { path: 'products', component: ProductPageComponent},
-  { path: 'company', component: CompanyPageComponent}];
+  { path: 'company', component: CompanyPageComponent},
+  { path: 'sale', component: SalePageComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules, relativeLinkResolution: 'legacy'})],
---
+++ fox-crm/src/app/pages/sale-page/sale-page.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SalePageComponent } from './sale-page.component';
+
+
+
+@NgModule({
+  declarations: [SalePageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SalePageModule { }
