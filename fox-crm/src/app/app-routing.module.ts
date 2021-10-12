169e7a6db08cca7e321f5232ccfa42ebfab2e9e2
Zsolf <zsolf57@gmail.com>
2021-10-13 00:42:10 +0200

    FCRM-001: Regisztracios oldal

--- fox-crm/src/app/app-routing.module.ts
+++ fox-crm/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './login/registration/registration.component';
 import { CompanyPageComponent } from './pages/main-page/company-page.component';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
 import { SalePageComponent } from './pages/sale-page/sale-page.component';
@@ -9,7 +10,8 @@ const routes: Routes = [
   { path: 'products', component: ProductPageComponent},
   { path: 'company', component: CompanyPageComponent},
   { path: 'sale', component: SalePageComponent},
-  { path: 'login', component: LoginComponent}];
+  { path: 'login', component: LoginComponent},
+  { path: 'registration', component: RegistrationComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules, relativeLinkResolution: 'legacy'})],
