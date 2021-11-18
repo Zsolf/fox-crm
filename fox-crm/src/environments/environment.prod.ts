34cd5034a453c54aad1d54026655709eb591fbfe
Zsolf <zsolf57@gmail.com>
2021-11-18 19:39:23 +0100

    FCRM-002: Ikon csere

--- fox-crm/src/environments/environment.prod.ts
+++ fox-crm/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyDLMoKbzJtuwiUeYXDqmtvpSN8bRAZ8gSE",
+    authDomain: "fox-crm-511f9.firebaseapp.com",
+    projectId: "fox-crm-511f9",
+    storageBucket: "fox-crm-511f9.appspot.com",
+    messagingSenderId: "748756701214",
+    appId: "1:748756701214:web:6e555258681674ca9e1e0f",
+    measurementId: "G-PPZ11NDVMV"
+  }
 };
